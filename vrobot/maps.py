e20a4e0a0e7134eed47ba5ee89491a02be1467fa
nicolasholland <hollandn@informatik.uni-freiburg.de>
2018-01-05 18:16:09 +0100

    Added vrobot

---
+++ vrobot/maps.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class map:
+
+    self.sides = None
+
+    def plot(self):
+        """
+        plot map
+        """
+        for side in self.sides:
+            plt.plot(side[:, 0], side[:, 1], color='black')
+
+class box(map):
+
+    """ a box """
+    def __init__(self, hlen=1):
+        s1 = np.array([-1, -1, -1, 1]).reshape(2,2)
+
+        s2 = np.array([1, -1, 1, 1]).reshape(2,2)
+
+        s3 = np.array([-1, -1, 1, -1]).reshape(2,2)
+
+        s4 = np.array([-1, 1, 1, 1]).reshape(2,2)
+
+
+        self.sides = np.array([s1, s2, s3, s4])
+
+
+if __name__ == '__main__':
+    b = box()
+    b.plot()
+
+    plt.show()
---
+++ vrobot/vrobot.py
@@ -0,0 +1,134 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from maps import box
+
+class vrobot:
+    """
+    A virtual robot.
+    """
+    def __init__(self, x, y):
+        """
+        Init robot with position.
+
+        Parameters
+        ----------
+        x : float
+        y : float
+        """
+        self.position = np.array([x, y])
+
+    def scan(self, nofrays=20, length=2):
+        """
+        Scan surroundings.
+
+        Parameters
+        ----------
+        nofrays : int, default 20
+        length : float, default 2
+        """
+        rays = np.array([[np.cos(x), np.sin(x)] for x in np.linspace(0, 2*np.pi,
+                                                                     nofrays)])
+        self.rays = rays * length
+
+    def reflect(self, maps):
+        """
+        compares rays from scan with current map to get intersections.
+
+        Parameters
+        ----------
+        maps : maps object
+        """
+        points = np.array([])
+        for i in range(len(self.rays)):
+            ray = np.array([self.position, self.rays[i]])
+            for j in range(len(maps.sides)):
+                wall = maps.sides[j]
+
+                inter = r.intersect(ray, wall)
+
+                if inter is not None:
+                    points = np.append(points, inter)
+
+        self.points = points.reshape(int(len(points)/2), 2)
+
+    def plot(self):
+        """
+        plot rays
+        """
+        for point in self.points:
+            plt.plot([self.position[0], point[0]], [self.position[1], point[1]],
+                     color='black')
+
+        for point in self.points:
+            plt.plot(point[0], point[1], 'o', color='red')
+
+        plt.plot(self.position[0], self.position[1], 'o', color='green')
+
+    def intersect(self, ray, wall, tol=.01):
+        """
+        compute intersection of ray and wall.
+
+        Parameters
+        ----------
+        ray : np.array
+        wall : np.array
+        """
+        assert(ray.shape == (2, 2))
+        assert(wall.shape == (2, 2))
+
+        L1 = self.__line(ray[0], ray[1])
+        L2 = self.__line(wall[0], wall[1])
+
+        R = self.__intersection(L1, L2)
+        if R:
+            sray1 = np.copy(ray[:, 0])
+            sray1.sort()
+
+            sray2 = np.copy(ray[:, 1])
+            sray2.sort()
+
+            swall1 = np.copy(wall[:, 0])
+            swall1.sort()
+
+            swall2 = np.copy(wall[:, 1])
+            swall2.sort()
+
+            if (R[0] > sray1[0] and R[0] < sray1[1] and
+                R[1] > sray2[0] and R[1] < sray2[1] and
+                R[0] > swall1[0]-tol and R[0] < swall1[1]+tol and
+                R[1] > swall2[0]-tol and R[1] < swall2[1]+tol):
+                return np.array(R)
+
+    @staticmethod
+    def __line(p1, p2):
+        A = (p1[1] - p2[1])
+        B = (p2[0] - p1[0])
+        C = (p1[0]*p2[1] - p2[0]*p1[1])
+        return A, B, -C
+
+    @staticmethod
+    def __intersection(L1, L2):
+        D  = L1[0] * L2[1] - L1[1] * L2[0]
+        Dx = L1[2] * L2[1] - L1[1] * L2[2]
+        Dy = L1[0] * L2[2] - L1[2] * L2[0]
+        if D != 0:
+            x = Dx / D
+            y = Dy / D
+            return x,y
+        else:
+            return False
+
+if __name__ == '__main__':
+    b = box()
+    r = vrobot(-.7, .5)
+
+    r.scan()
+    r.reflect(b)
+
+    b.plot()
+    r.plot()
+
+
+    plt.axis([-2, 2, -2, 2])
+    plt.show()
