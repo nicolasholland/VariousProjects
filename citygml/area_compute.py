c3dacbec275cb244f200241c423bae7b0b64471a
nholland <nholland@de04522.ise.fhg.de>
2018-10-24 15:35:01 +0200

    added area_compute.py

---
+++ citygml/area_compute.py
@@ -0,0 +1,14 @@
+"""
+area_compute.py
+---------------
+
+Loop over roof tops, compute areas and write into file.
+Use with:
+
+$ blender -b -P area_compute.py
+"""
+import bpy
+
+bpy.ops.wm.addon_enable(module="object_print3d_utils")
+
+
