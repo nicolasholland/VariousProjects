ee7a3a0a3bb1eb3cf58cdab95b6948f8e8795f15
nicolas holland <dutchman@localhost.localdomain>
2018-12-09 17:42:51 +0100

    Envelope is mostly working now!

--- envelope/envelope_pandastype.py
+++ envelope/envelope_pandastype.py
@@ -5,9 +5,9 @@ Created on Sun Dec  2 21:42:15 2018
 
 @author: dutchman
 
-Doesnt work yet...
-Im missing something.
+My envelope pandas extension.
 """
+import unittest
 import numpy as np
 import pandas as pd
 from pandas.api.extensions import ExtensionDtype
@@ -38,28 +38,30 @@ class EnvelopeArray(ExtensionArray):
     """
     _can_hold_na = True
 
-    def __init__(self, array, dtype=None, copy=None):
-        #assert array.shape[1] == 2
-        #assert (array[:, 0] <= array[:, 1]).all()
-        self.data = array
-        self.lower = array[:, 0]
-        self.upper = array[:, 1]
+    def __init__(self, array, upper=None, dtype=None, copy=None):
+        self.lower = array
+
+        if upper is None:
+            upper = array * np.nan
+        else:
+            if not (array < upper).all():
+                raise ValueError("Upper array must be greater than lower.")
+
+        self.upper = upper
+
 
     @classmethod
     def _from_factorized(cls, values, original):
         return cls(values)
 
     def __str__(self):
-        return str(self.data)
+        return  ''.join(['l', str(self.lower), ' u', str(self.upper)])
 
     def __repr__(self):
         return self.__str__()
 
     def __getitem__(self, item):
-        retval = type(self)(self.data[item])
-        if self.data.shape == (2,):
-            retval = type(self)(self.data)
-        #print("I AM CALLED getitem ", "item: ", item, "retval: ", retval, "shape ", self.data.shape)
+        retval = type(self)(self.lower[item], upper=self.upper[item])
         return retval
 
     def __len__(self):
@@ -69,15 +71,11 @@ class EnvelopeArray(ExtensionArray):
         -------
         length : int
         """
-        retval = self.data.shape[0]
-        if len(self.data.shape) == 1:
-            retval = 1
-        #print("Are you looking at me? ", "shape: ", self.data.shape, "retval ", retval)
-        return retval
+        return len(self.lower)
 
     @property
     def dtype(self):
-        return EnvelopeType() 
+        return EnvelopeType()
 
     @classmethod
     def _concat_same_type(cls, to_concat):
@@ -90,14 +88,15 @@ class EnvelopeArray(ExtensionArray):
         -------
         ExtensionArray
         """
-        return cls(np.concatenate(ea.data for ea in to_concat))
+        data = np.concatenate(ea.data for ea in to_concat)
+        upper = np.concatenate(ea.upper for ea in to_concat)
+        return cls(data, upper=upper)
 
     def isna(self):
         """
         Boolean NumPy array indicating if each value is missing.
         """
-        print("Is this happening?")
-        return (self.data[:, 0] == np.nan) & (self.data[:, 1] == np.nan)
+        return np.isnan(self.lower) & np.isnan(self.upper)
 
     def copy(self, deep=False):
         """
@@ -111,18 +110,20 @@ class EnvelopeArray(ExtensionArray):
         ExtensionArray
         """
         if deep:
-            return type(self)(self.data.copy(deep=True))
-        return type(self)(self.data)
+            return type(self)(self.lower.copy(deep=True))
+        return type(self)(self.lower)
 
-    def __array2__(self):
-        return self.upper
+    @property
+    def T(self):
+        return self
+
 
     @property
     def nbytes(self):
         """
         The number of bytes needed to store this object in memory.
         """
-        return self.data.nbytes
+        return self.lower.nbytes + self.upper.nbytes
 
 
     @classmethod
@@ -143,6 +144,7 @@ class EnvelopeArray(ExtensionArray):
     def take(self, indices, allow_fill=False, fill_value=None):
         """
         Take elements from an array.
+
         Parameters
         ----------
         indices : sequence of integers
@@ -164,28 +166,24 @@ class EnvelopeArray(ExtensionArray):
             physical NA value. `fill_value` should be the user-facing version,
             and the implementation should handle translating that to the
             physical version for processing the take if nescessary.
+
         Returns
         -------
         ExtensionArray
-        Raises
-        ------
-        IndexError
-            When the indices are out of bounds for the array.
-        ValueError
-            When `indices` contains negative values other than ``-1``
-            and `allow_fill` is True.
+
         Notes
         -----
         ExtensionArray.take is called by ``Series.__getitem__``, ``.loc``,
         ``iloc``, when `indices` is a sequence of values. Additionally,
         it's called by :meth:`Series.reindex`, or any other method
         that causes realignemnt, with a `fill_value`.
+
         See Also
         --------
         numpy.take
         pandas.api.extensions.take
         """
-        return type(self)(self.data[indices])
+        return type(self)(self.lower[indices], upper=self.upper[indices])
 
 
 @pd.api.extensions.register_series_accessor("envelope")
@@ -195,19 +193,44 @@ class EnvelopeAccessor:
         self._index = obj.index
         self._name = obj.name
 
-
-bound_array = np.array([[2,3], [3,4], [4,5]])
-#bound_array = np.array([1,2])
-
-c = EnvelopeArray(bound_array)
-print("Im an array ", c)
-
-#c = EnvelopeArray((2,3))
+    def __call__(self):
+        if type(self._data) == EnvelopeArray:
+            return self._data.lower, self._data.upper
+        else:
+            raise ValueError("Only works for EnvelopeArrays")
 
 
-print("make a dataframe")
-#df = pd.DataFrame({'aha': c})
-df = pd.Series(c)
+class EnvelopeArrayTest(unittest.TestCase):
+    def test_init(self):
+        lower = np.array([1, 2, 3])
+        upper = np.array([2, 3, 4])
 
-#print(df)
+        ea = EnvelopeArray(lower, upper=upper)
+        self.assertTrue(isinstance(ea, EnvelopeArray))
+    def test_series(self):
+        lower = np.array([1, 2, 3])
+        upper = np.array([2, 3, 4])
+
+        ts = pd.Series(EnvelopeArray(lower, upper=upper))
+        self.assertTrue(isinstance(ts, pd.Series))
+
+    def test_dataframe(self):
+        lower = np.array([1, 2, 3])
+        upper = np.array([2, 3, 4])
+
+        df = pd.DataFrame({'an_envelope': EnvelopeArray(lower, upper=upper)})
+        self.assertTrue(isinstance(df, pd.DataFrame))
+
+    def test_accessor(self):
+        lower = np.array([1, 2, 3])
+        upper = np.array([2, 3, 4])
+
+        ts = pd.Series(EnvelopeArray(lower, upper=upper))
+        minarr, maxarr = ts.envelope()
+        self.assertTrue(np.equal(minarr, lower).all())
+        self.assertTrue(np.equal(maxarr, upper).all())
+
+if __name__ == '__main__':
+    unittest.main()
+
 
--- envelope/whatIdid.md
+++ envelope/whatIdid.md
@@ -1,33 +1,95 @@
 Forecast Envelope
 =================
 
+A while ago we saw a talk about adding a fast string type into pandas using arrow and numba.
+Besides the fact how awesome that is, we were fascinated by the idea of adding custom data types into pandas.
+So we decided to try and implement our own custom data type.
 
-To read financial data:
-[pandas-datareader](https://pandas-datareader.readthedocs.io/en/latest/remote_data.html#iex)
+In order to learn how to do that we looked at the package from the talk [fletcher](), as well as some other examples
+like [cyberpandas](), which implements a custom data type for ip addresses.
+And of course there is a documentation page by [pandas](https://pandas.pydata.org/pandas-docs/stable/extending.html) that also explains how to do that.
+
+
+But before we could implement a data type we had to come up with one.
+So we thought of a problem that a new data type could help in solving.
+Let's assume it is our business to forecast stock values.
+
+We can download stock values using the [pandas-datareader](https://pandas-datareader.readthedocs.io/en/latest/remote_data.html#iex).
+As an example we use the google stock GOOG.
 
 ```
-$ conda install pandas-datareader
+>>> from pandas_datareader import data
+>>> df = data.DataReader('GOOG', start='2017-11-01', end='2018-11-01', data_source='iex')
 ```
 
-The pandas-datareader allows us to simply download stock records into dataframes.
-As an example we'll be using the google stock GOOG.
+We want to forecast the 'closed' stock value for the day ahead.
+In order to create the forecasts we take a time series of the last 30 days before the day we want to forecast.
+Then we create a bunch of sklearn regressors and fit them to the timeseries e.g. [support vector regression](https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVR.html)
+Once we have the fitted curves we extrapolate them by one step and now we have an assemble of forecasts.
+
+Here is how our fits and forecasts may look like:
+
+PICTURE
 
 
-Create a bunch of sklearn regressors and fit them to the timeseries.
-Use regressors to make predictions.
-e.g. [support vector regression](https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVR.html)
+We know that our forecasts themselfs are not reliable, so instead of using them directly we look at their minmax range.
+This minmax range is the subject of our imaginary business.
+We create those range forecasts for stocks and therefore predict how the stocks may develop.
+And since ideally the stock curves should be within those ranges we call them envelopes.
+Here is an example of the envelope created by our sklearn regressor forecasts:
 
-Create envelope of predictions.
-Use seaborn to plot.
-Like [this](https://seaborn.pydata.org/examples/errorband_lineplots.html)
-
-Create an envelope data type in pandas like
-
-Explained [here](https://pandas.pydata.org/pandas-docs/stable/extending.html)
+PICTURE
 
 
+Even though we use an ansemble of forecasts to create the envelopes, an envelope itself can be stored with just two arrays.
+Since these envelopes are our business, it is important to us that they are reasonable,
+e.g. there must be no envelope where the max curve is lower than the min curve.
+There are many ways to ensure this property e.g. we could add constraints to our database, or add assertions to the forecasting process.
+But to be really sure that the envelopes are ok, we create a custom envelope data type, which we use to extend pandas!
 
+There are two pandas classes that we need to subclass for that, ExtensionDtype and ExtensionArray
 
+```
+>>> from pandas.api.extensions import ExtensionDtype
+>>> from pandas.core.arrays import ExtensionArray
+```
 
+The pandas documentation tells us what methods and attributes we have to overwrite.
+Note that the classes are not abc, so there is no imideat??? way to see if you've implemented everything you need.
+Since our data type is basically just two numpy arrays, we learned a lot from pandas' categorical array [pandas.categorical](https://github.com/pandas-dev/pandas/blob/v0.23.4/pandas/core/arrays/categorical.py#L170-L2343).
+We put our minmax constraint right in the constructor:
 
+```
+def __init__(self, array, upper=None, dtype=None, copy=None):
+    ...
+    if not (self.lower < upper).all():
+        raise ValueError("Upper array must be greater than lower.")
+    ...
+```
+
+Here is how our Extension Array looks in action:
+
+```
+>>> minarray, maxarray = np.array([14, 9, 12]), np.array([15, 13, 17])
+>>> df = pd.DataFrame({'an_envelope': EnvelopeArray(minarray, upper=maxarray)})
+>>> df
+  an_envelope
+0     l14 u15
+1      l9 u13
+2     l12 u17
+```
+
+Now we can create dataframes with forecast envelopes!
+Different stocks can have different columns and we can use many of pandas great features. =D
+We also learned how to register cusom accessors for both Series and DataFrame objects.
+Basically you create an accessor class, that does what ever you want and you decorate it with pandas' accessor decorators:
+
+```
+@pd.api.extensions.register_series_accessor("envelope")
+class EnvelopeAcessor:
+    ...
+```
+
+We Added an envelope accessor that makes it easy to retrieve the underlying lower and upper numpy arrays from our envelope arrays.
+You can find the code on our [github]()
 
